44415c8c7d3f21c5e61c6273326f04653ba5b4c0
Marcio Toshio <contato@marciotoshio.com.br>
2012-02-02 01:55:50 -0200

    no need to be instance variables

--- server/pegapega.rb
+++ server/pegapega.rb
@@ -3,31 +3,31 @@ require 'em-websocket'
 require_relative 'lib/game'
 
 module PegaPega
-	@game = Game.new 690, 400
-	@port = 30000
+	game = Game.new 690, 400
+	port = 30000
 	
 	def self.debug_message(msg)
 		puts "PegaPega => #{msg}"
 	end
 
 	EventMachine.run do
-		EventMachine::WebSocket.start(:host => '0.0.0.0', :port => @port, :debug => false) do |client|
+		EventMachine::WebSocket.start(:host => '0.0.0.0', :port => port, :debug => false) do |client|
 			
 			client.onopen do
 				debug_message "client connected: " + client.to_s
 			end
 		  
 			client.onmessage do |msg|
-				@game.join(client, msg) if msg.include? "[join]"
-				@game.move(client, msg) if msg.include? "[move]"
-				@game.check_collision
-				@game.send_players_info
+				game.join(client, msg) if msg.include? "[join]"
+				game.move(client, msg) if msg.include? "[move]"
+				game.check_collision
+				game.send_players_info
 				debug_message "message received: " + msg
 			end
 		  
 			client.onclose do
-				@game.remove_player client
-				@game.send_players_info
+				game.remove_player client
+				game.send_players_info
 				debug_message "bye " + client.to_s
 			end
 		
@@ -37,6 +37,6 @@ module PegaPega
 				end
 			end
 		end	
-		debug_message "Server Up. Runnning on port #{@port}"
+		debug_message "Server Up. Runnning on port #{port}"
 	end
 end
